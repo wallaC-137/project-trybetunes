8084b028391b0544077824345e4f628a91c914d1
Wallace <wallace_031@hotmail.com>
2022-12-13 19:44:34 -0300

    ✅-1. Crie as rotas necessárias para a aplicação

---
+++ src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        teste
+      </div>
+    );
+  }
+}
+
+export default Search;
