80f8182a6c565b1b8f1c959086dc52af45aa269e
Wallace <wallace_031@hotmail.com>
2022-12-14 12:06:44 -0300

    2. Crie um formulário para identificação 🚀

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,77 @@
 import React from 'react';
+import PropType from 'prop-types';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    btn: true,
+    name: '',
+    loading: false,
+  };
+
+  hendleSaveState = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.checkField);
+  };
+
+  checkField = () => {
+    const { name } = this.state;
+    const validation = 3;
+
+    this.setState({
+      btn: name.length < validation,
+    });
+  };
+
+  lsSaveName = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+
+    this.setState({
+      loading: true,
+    });
+
+    await createUser({ name });
+    history.push('/search');
+  };
+
   render() {
+    const { btn, loading } = this.state;
+
     return (
       <div data-testid="page-login">
-        teste
+        { loading && <Loading />}
+        <form>
+          <label htmlFor="name">
+            nome:
+            <input
+              type="text"
+              name="name"
+              id="name"
+              data-testid="login-name-input"
+              onChange={ this.hendleSaveState }
+            />
+          </label>
+          <input
+            type="button"
+            value="Entrar"
+            data-testid="login-submit-button"
+            disabled={ btn }
+            onClick={ this.lsSaveName }
+          />
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropType.shape({
+    push: PropType.func,
+  }),
+}.isRequired;
 
 export default Login;
