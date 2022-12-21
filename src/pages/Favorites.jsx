9131808c36b0d4d232036e67a27e464097010d7a
Wallace <wallace_031@hotmail.com>
2022-12-21 11:07:25 -0300

    problema do ultimo quisitor do requisito 12, remove a musica do localstorage e da tela, mas não passa no teste

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends React.Component {
+  state = {
+    load: true,
+    result: [],
+    favorit: true,
+  };
+
+  async componentDidMount() {
+    const resultApi = await getFavoriteSongs();
+    // const checkFav = await checkFavorite();
+
+    console.log(resultApi);
+    this.setState({
+      result: resultApi,
+      load: false,
+      // favorit: true,
+    });
+  }
+
+  async componentDidUpdate() {
+    const resultApi = await getFavoriteSongs();
+
+    this.setState({
+      result: resultApi,
+      // favorit: true,
+    });
+  }
+
   render() {
+    const { load, result, favorit } = this.state;
+
     return (
       <div data-testid="page-favorites">
         <Header />
-        fav
+        { load && <Loading /> }
+        { (result || []).map((music) => (<MusicCard
+          key={ music.trackId }
+          { ...music }
+          isFavorite={ favorit }
+        />))}
       </div>
     );
   }
