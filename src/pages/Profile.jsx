de78954e8e8c5a44cee588edf566dfde218287cb
Wallace <wallace_031@hotmail.com>
2022-12-26 10:59:37 -0300

    vai?

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    // user: {},
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+    load: true,
+  };
+
+  async componentDidMount() {
+    const { name, email, description, image } = await getUser();
+
+    this.setState({
+      name,
+      email,
+      description,
+      image,
+      load: false,
+    });
+  }
+
   render() {
+    const { load, name, email, description, image } = this.state;
+    // console.log(name, email, description, image);
+
     return (
       <div data-testid="page-profile">
         <Header />
-        prof
+        { load && <Loading /> }
+        <div>
+          <img src={ image } alt={ name } data-testid="profile-image" />
+          <button type="button"><Link to="/profile/edit">Editar perfil</Link></button>
+        </div>
+        <h3>{ name }</h3>
+        <p>{ email }</p>
+        <p>{ description }</p>
       </div>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,82 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+    load: true,
+  };
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({
+      name: user.name,
+      email: user.email,
+      description: user.description,
+      image: user.image,
+      load: false,
+    });
+    // console.log(user);
+  }
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { load, name, email, description, image } = this.state;
+
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        { load && <Loading /> }
+        <form>
+          <input
+            placeholder="Nome"
+            type="text"
+            name="name"
+            id="name"
+            value={ name }
+            data-testid="edit-input-name"
+            onChange={ this.handleChange }
+          />
+          <input
+            placeholder="Email"
+            type="text"
+            name="email"
+            id="email"
+            value={ email }
+            data-testid="edit-input-email"
+            onChange={ this.handleChange }
+          />
+          <input
+            placeholder="Descrição"
+            type="text"
+            name=""
+            id=""
+            value={ description }
+            data-testid="edit-input-description"
+            onChange={ this.handleChange }
+          />
+          <input
+            placeholder="Url da imagem"
+            type="text"
+            name=""
+            id=""
+            value={ image }
+            data-testid="edit-input-image"
+            onChange={ this.handleChange }
+          />
+          <input type="button" value="Salvar" />
+        </form>
       </div>
     );
   }
